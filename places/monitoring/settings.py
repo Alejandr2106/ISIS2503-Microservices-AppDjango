156f021a467d163aa54f04d63628d5c4c79b16f9
Alejandr2106 <a.gonzalez48@uniandes.edu.co>
2023-11-28 23:51:28 -0500

    .

--- places/monitoring/settings.py
+++ places/monitoring/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'variables_db',
-        'USER': 'variables_user',
+        'NAME': 'places_db',
+        'USER': 'places_user',
         'PASSWORD': 'isis2503',
         'HOST': '0.0.0.0',
         'PORT': '5432',
